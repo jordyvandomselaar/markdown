143bcb5c4a724afa9a92adee95af4bd87ce0b4b4
Jordy van Domselaar <jordy@deinternetjongens.nl>
2019-01-02 13:04:23 +0100

    [CHORE]: prettier

--- src/components/MarkdownEditor/index.js
+++ src/components/MarkdownEditor/index.js
@@ -1,9 +1,9 @@
-import React, {useEffect} from 'react';
-import 'codemirror/lib/codemirror.css'; // codemirror
-import 'tui-editor/dist/tui-editor.css'; // editor ui
-import 'tui-editor/dist/tui-editor-contents.css'; // editor content
-import 'highlight.js/styles/github.css'; // code block highlight
-import Editor from 'tui-editor';
+import React, { useEffect } from "react";
+import "codemirror/lib/codemirror.css"; // codemirror
+import "tui-editor/dist/tui-editor.css"; // editor ui
+import "tui-editor/dist/tui-editor-contents.css"; // editor content
+import "highlight.js/styles/github.css"; // code block highlight
+import Editor from "tui-editor";
 
 const MarkdownEditor = ({ onMarkdownChange, initialMarkdown }) => {
   let editorInstance;
